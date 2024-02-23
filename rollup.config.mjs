5752bc67d5e5690c618a32d8827bd2a51943193a
bitbeckers <code@bitbeckers.com>
2024-02-23 15:10:34 +0100

    fix(build): build with rollup and sentry sourcemaps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,26 @@
 // rollup.config.js
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { sentryRollupPlugin } from "@sentry/rollup-plugin";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const config = {
-  input: "src/index.ts",
+  external: "@sentry/profiling-node",
+  input: "src/server.ts",
   output: {
     dir: "dist",
     format: "es",
     sourcemap: true, // Source map generation must be turned on
   },
   plugins: [
-    nodeResolve(),
+    nodeResolve({ preferBuiltins: true }),
     typescript(),
+    commonjs({ defaultIsModuleExports: true }),
+    json(),
     sentryRollupPlugin({
       authToken: process.env.SENTRY_AUTH_TOKEN,
       org: "hypercerts",
