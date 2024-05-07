02f1b14718c3be62057f3408ca634e2faa6e9964
jipstavenuiter <jipstavenuiter@gmail.com>
2024-05-07 16:31:12 -0400

    (feature): add some validation and update tests

--- test/storage/storeHypercert.test.ts
+++ test/storage/storeHypercert.test.ts
@@ -9,47 +9,41 @@ import { faker } from "@faker-js/faker";
 import { supabaseUrl } from "../../src/utils/constants";
 
 describe("storeHypercert", {}, async () => {
+  const claim: NewClaim = {
+    contract_id: faker.string.uuid(),
+    uri: "ipfs://metadataCIDstoreHypercert",
+    contract_address: default_contractAddress,
+    creator_address: faker.finance.ethereumAddress(),
+    token_id: 1n,
+    units: 1n,
+    block_timestamp: 1n,
+  };
+
   it("store hypercert data  in DB", {}, async () => {
     server.use(
       http.post(`${supabaseUrl}/*`, () => {
         return HttpResponse.json([mockMetadata]);
       }),
     );
-    const claim: NewClaim = {
-      contract_id: default_contractAddress,
-      // contractAddress: default_contractAddress as `0x${string}`,
-      // claimID: 1n,
-      uri: "ipfs://metadataCIDstoreHypercert",
-      // metadata: mockMetadata,
-      contract_address: default_contractAddress,
-      creator_address: faker.finance.ethereumAddress(),
-      token_id: 1n,
-      units: 1n,
-      block_timestamp: 1n,
-    };
 
     const storedClaim = await storeClaim({
       claims: [claim],
     });
 
     expect(storedClaim).toBeUndefined();
-
-    // expect(storedClaim?.contract_address).toEqual(default_contractAddress);
   });
 
   it("should throw an error if creator address is invalid", async () => {
-    const claim = {
-      contract_address: default_contractAddress,
+    const claimWithWrongAddress = {
+      ...claim,
       creator_address: "invalid address",
-      token_id: 1n,
-      units: 1n,
-      block_timestamp: 1n,
     } as unknown as NewClaim;
 
-    expect(async () => {
-      await storeClaim({
-        claims: [claim],
-      });
-    }).rejects.toThrowError();
+    await expect(
+      async () =>
+        await storeClaim({
+          claims: [claimWithWrongAddress],
+        }),
+    ).rejects.toThrowError("[StoreClaim] Invalid creator address");
   });
 });
