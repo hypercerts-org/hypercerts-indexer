1fe65b7cea085080fce2ec763248f5170126ac9e
bitbeckers <code@bitbeckers.com>
2024-06-13 15:31:43 +0200

    fix(claimId): prevent multiple claimIds in batch transfer processing

--- supabase/migrations/20240612164643_update_unit_transfer_method.sql
+++ supabase/migrations/20240612164643_update_unit_transfer_method.sql
@@ -7,9 +7,11 @@ DECLARE
     transfer   transfer_units_type;
     from_token fractions%ROWTYPE;
     to_token   fractions%ROWTYPE;
+    row        fractions%ROWTYPE;
 BEGIN
 
     -- Create an intermediate table
+
     CREATE TEMPORARY TABLE IF NOT EXISTS intermediate_fractions AS TABLE fractions WITH NO DATA;
 
     -- Add a unique constraint on token_id and claims_id
@@ -19,76 +21,82 @@ BEGIN
     -- Parse all the transfers into the intermediate table
     FOR transfer IN SELECT * FROM unnest(p_transfers)
         LOOP
-            -- get from_token
+            -- Assert claim_id and token_id in transfer is not null else throw
+            IF transfer.claim_id IS NULL OR transfer.from_token_id IS NULL OR transfer.to_token_id IS NULL THEN
+                RAISE EXCEPTION 'claim_id % , from_token_id %, to_token_id % cannot be null', transfer.claim_id, transfer.from_token_id, transfer.to_token_id;
+            END IF;
+
+            -- Check if a matching record exists in the fractions table
             IF EXISTS (SELECT 1
                        FROM fractions
-                       WHERE fractions.token_id = transfer.from_token_id
-                         AND fractions.claims_id = transfer.claim_id) THEN
+                       WHERE token_id = transfer.from_token_id
+                         AND claims_id = transfer.claim_id) THEN
                 SELECT *
                 INTO from_token
                 FROM fractions
-                WHERE fractions.token_id = transfer.from_token_id
-                  AND fractions.claims_id = transfer.claim_id
-                    FOR UPDATE;
+                WHERE token_id = transfer.from_token_id
+                  AND claims_id = transfer.claim_id FOR UPDATE;
 
-                INSERT INTO intermediate_fractions (claims_id, token_id, units, last_block_update_timestamp)
+                -- Insert or update the record in the intermediate_fractions table
+                INSERT INTO intermediate_fractions (claims_id, token_id, units, creation_block_timestamp,
+                                                    last_block_update_timestamp)
                 VALUES (from_token.claims_id, from_token.token_id,
-                        from_token.units, from_token.last_block_update_timestamp)
-                ON CONFLICT (token_id, claims_id)
-                    DO UPDATE SET units                       = COALESCE(intermediate_fractions.units, 0) -
-                                                                transfer.units_transferred,
-                                  last_block_update_timestamp = transfer.block_timestamp;
+                           -- units could be null, so we need to coalesce it to 0
+                        COALESCE(from_token.units, 0),
+                        from_token.creation_block_timestamp,
+                        from_token.last_block_update_timestamp)
+                ON CONFLICT (token_id, claims_id) DO NOTHING;
             END IF;
 
             -- get or create to_token
             IF EXISTS (SELECT 1
                        FROM fractions
-                       WHERE fractions.token_id = transfer.to_token_id
-                         AND fractions.claims_id = transfer.claim_id) THEN
+                       WHERE token_id = transfer.to_token_id
+                         AND claims_id = transfer.claim_id) THEN
                 SELECT *
                 INTO to_token
                 FROM fractions
-                WHERE fractions.token_id = transfer.from_token_id
-                  AND fractions.claims_id = transfer.claim_id
-                    FOR UPDATE;
+                WHERE token_id = transfer.to_token_id
+                  AND claims_id = transfer.claim_id FOR UPDATE;
 
-                INSERT INTO intermediate_fractions (claims_id, token_id, units, last_block_update_timestamp)
-                VALUES (to_token.claims_id, to_token.token_id,
-                        to_token.units, to_token.last_block_update_timestamp)
-                ON CONFLICT (token_id, claims_id)
-                    DO UPDATE SET units                       = COALESCE(intermediate_fractions.units, 0) -
-                                                                transfer.units_transferred,
-                                  last_block_update_timestamp = transfer.block_timestamp;
+                INSERT INTO intermediate_fractions (claims_id, token_id, units, creation_block_timestamp,
+                                                    last_block_update_timestamp)
+                VALUES (to_token.claims_id, to_token.token_id, COALESCE(to_token.units, 0),
+                        to_token.creation_block_timestamp, to_token.last_block_update_timestamp)
+                ON CONFLICT (token_id, claims_id) DO NOTHING;
+
             ELSE
                 INSERT INTO intermediate_fractions (claims_id, token_id, creation_block_timestamp, units,
                                                     last_block_update_timestamp)
-                VALUES (transfer.claim_id, transfer.to_token_id, 0,
-                        transfer.block_timestamp,
-                        transfer.block_timestamp);
+                VALUES (transfer.claim_id, transfer.to_token_id, transfer.block_timestamp, 0, transfer.block_timestamp);
+
                 SELECT *
                 INTO to_token
                 FROM intermediate_fractions
-                WHERE intermediate_fractions.token_id = transfer.to_token_id
-                  AND intermediate_fractions.claims_id = transfer.claim_id
-                    FOR UPDATE;
+                WHERE token_id = transfer.to_token_id
+                  AND claims_id = transfer.claim_id FOR UPDATE;
             END IF;
 
-            -- Insert into intermediate table instead of updating fractions table
-            INSERT INTO intermediate_fractions (claims_id, token_id, units, last_block_update_timestamp)
-            VALUES (transfer.claim_id, transfer.from_token_id,
-                    COALESCE(from_token.units, 0) - transfer.units_transferred, transfer.block_timestamp)
-            ON CONFLICT (token_id, claims_id)
-                DO UPDATE SET units                       = COALESCE(intermediate_fractions.units, 0) -
-                                                            transfer.units_transferred,
-                              last_block_update_timestamp = transfer.block_timestamp;
+            -- Update from_token units if tokenId is not 0
+            IF transfer.from_token_id != 0 THEN
+                INSERT INTO intermediate_fractions (claims_id, token_id, units, last_block_update_timestamp)
+                VALUES (transfer.claim_id, transfer.from_token_id,
+                        COALESCE(from_token.units, 0) - transfer.units_transferred, transfer.block_timestamp)
+                ON CONFLICT (token_id, claims_id)
+                    DO UPDATE SET units                       = from_token.units - transfer.units_transferred,
+                                  last_block_update_timestamp = transfer.block_timestamp;
+            END IF;
 
-            INSERT INTO intermediate_fractions (claims_id, token_id, units, last_block_update_timestamp)
-            VALUES (transfer.claim_id, transfer.to_token_id, COALESCE(to_token.units, 0) + transfer.units_transferred,
-                    transfer.block_timestamp)
-            ON CONFLICT (token_id, claims_id)
-                DO UPDATE SET units                       = COALESCE(intermediate_fractions.units, 0) +
-                                                            transfer.units_transferred,
-                              last_block_update_timestamp = transfer.block_timestamp;
+            -- Update to_token units if tokenId is not 0
+            IF transfer.to_token_id != 0 THEN
+                INSERT INTO intermediate_fractions (claims_id, token_id, units, last_block_update_timestamp)
+                VALUES (transfer.claim_id, transfer.to_token_id, to_token.units + transfer.units_transferred,
+                        transfer.block_timestamp)
+                ON CONFLICT (token_id, claims_id)
+                    DO UPDATE SET units                       = to_token.units +
+                                                                transfer.units_transferred,
+                                  last_block_update_timestamp = transfer.block_timestamp;
+            END IF;
 
         END LOOP;
 
@@ -98,13 +106,12 @@ BEGIN
     WHERE token_id = 0;
 
     -- TODO handle upsert from intermediate table to fractions table
-    -- Update fractions table from intermediate table
-    UPDATE fractions
-    SET units                       = intermediate_fractions.units,
-        last_block_update_timestamp = intermediate_fractions.last_block_update_timestamp
+    INSERT INTO fractions (claims_id, token_id, units, last_block_update_timestamp)
+    SELECT claims_id, token_id, units, last_block_update_timestamp
     FROM intermediate_fractions
-    WHERE fractions.token_id = intermediate_fractions.token_id
-      AND fractions.claims_id = intermediate_fractions.claims_id;
+    ON CONFLICT (token_id, claims_id)
+        DO UPDATE SET units                       = EXCLUDED.units,
+                      last_block_update_timestamp = EXCLUDED.last_block_update_timestamp;
 
     -- Drop the intermediate table
     DROP TABLE intermediate_fractions;
