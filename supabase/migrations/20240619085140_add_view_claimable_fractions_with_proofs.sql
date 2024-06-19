732f5ea6c22d6686bebec1b01180232480d17c17
bitbeckers <code@bitbeckers.com>
2024-06-19 11:43:36 +0200

    feat(allowlist): view for claiming fractions

---
+++ supabase/migrations/20240619085140_add_view_claimable_fractions_with_proofs.sql
@@ -0,0 +1,14 @@
+create view claimable_fractions_with_proofs as
+select halr.id,
+       token_id,
+       leaf,
+       entry,
+       user_address,
+       claimed,
+       proof,
+       halr.units,
+       claims.hypercert_id,
+       claims.units as total_units
+from claims
+         join public.hypercert_allow_lists hal on claims.id = hal.claims_id
+         join public.hypercert_allow_list_records halr on hal.id = halr.hypercert_allow_lists_id;
