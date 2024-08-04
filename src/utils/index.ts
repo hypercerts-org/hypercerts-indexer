4eba42d6636e5cfb1c71e4217e5e3ab629d3d832
bitbeckers <code@bitbeckers.com>
2024-08-05 00:29:42 +0200

    fix(seed): remove last_block_updated from supported schemas

--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import { assertExists } from "./assertExists.js";
-import { getDeployment } from "./getDeployment.js";
-import { fetchFromHTTPS, fetchFromIPFS } from "./fetching.js";
-
-export { assertExists, getDeployment, fetchFromIPFS, fetchFromHTTPS };
